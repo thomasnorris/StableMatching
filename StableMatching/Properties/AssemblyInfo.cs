3233916cc6c6755dd285f5ae3f4416addc4d2f84
thomasnorris <tom.norris135@gmail.com>
2017-10-13 15:36:36 -0400

    more renaming because I am an idiot

--- StableMatching/Properties/AssemblyInfo.cs
+++ StableMatching/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StableMatching-CSharp")]
+[assembly: AssemblyTitle("StableMatching")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("StableMatching-CSharp")]
+[assembly: AssemblyProduct("StableMatching")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
